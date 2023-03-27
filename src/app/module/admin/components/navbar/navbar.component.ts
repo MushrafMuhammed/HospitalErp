e8aa86a6b887d33813a4861023c96f49a73ce104
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2023-03-27 15:52:28 +0530

    after moonjal

---
+++ src/app/module/admin/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  // @Input()
+  // @Output()
+
+}
